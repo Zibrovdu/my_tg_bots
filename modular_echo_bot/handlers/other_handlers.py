f5fd7a5ed586997e89902eaea66d076251924d85
Dmitriy Zibrov <zibrovdu@gmail.com>
2024-04-26 20:38:44 +0300

    init commit

---
+++ modular_echo_bot/handlers/other_handlers.py
@@ -0,0 +1,16 @@
+from aiogram.types import Message
+from lexicon.lexicon import LEXICON_RU
+from aiogram import Router
+
+# Инициализируем роутер уровня модуля
+router = Router()
+
+
+# Этот хэндлер будет срабатывать на любые ваши сообщения,
+# кроме команд "/start" и "/help"
+@router.message()
+async def echo(message: Message):
+    try:
+        await message.send_copy(chat_id=message.chat.id)
+    except TypeError:
+        await message.reply(text=LEXICON_RU['no_echo'])
